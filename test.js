89a993b4da7dff216091ac7dd184625ef8adcd07
MoonSupport <jiwon3346@naver.com>
2020-08-24 23:53:52 +0900

    구조 변경

--- test.js
+++ test.js
@@ -1,45 +1,19 @@
+const assert = require('assert')
 
-module.exports = Test;
+describe('number', function(){
+      it('compare 1 and 1', function(){
+        assert.equal(1,1)
+      }); 
+      it('sum 1 and 1', function(){
+        assert.equal(1+1,2)
+      }); 
+});
 
-function Test(title, fn) {
-  this.title = title;
-  this.fn = fn;
-  this.async = !! fn.length;
-  this.sync = ! this.async;
-  this.timeout(5000);
-}
-
-Test.prototype.timeout = function(ms){
-  this._timeout = ms;
-  return this;
-};
-
-Test.prototype.fullTitle = function(){
-  return this.parent.fullTitle() + ' ' + this.title;
-};
-
-Test.prototype.run = function(fn){
-  var timer
-    , self = this
-    , ms = this._timeout
-    , start = new Date;
-
-  if (this.async) {
-    timer = setTimeout(function(){
-      fn(new Error('timeout of ' + ms + 'ms exceeded'));
-    }, ms);
-  }
-
-  // async
-  if (this.async) {
-    this.fn(function(err){
-      clearTimeout(timer);
-      self.duration = new Date - start;
-      fn(err);
-    });
-  // sync
-  } else {
-    this.fn();
-    this.duration = new Date - start;
-  }
-};
+describe('string', function(){
+    it('compare 1 and 1', function(){
+      assert.equal('1','1')
+      }); 
+      it('compare a and a', function(){
+        assert.equal('a','a')
+      }); 
+});
